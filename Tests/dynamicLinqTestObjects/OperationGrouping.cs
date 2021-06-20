bc65882adf377d6d876102ebeebe6426d21a795e
Zev Spitz <shivisi@hotmail.com>
2021-06-20 07:29:28 +0300

    Fix #84

---
+++ Tests/dynamicLinqTestObjects/OperationGrouping.cs
@@ -0,0 +1,12 @@
+﻿using ExpressionTreeTestObjects;
+using System.Linq.Expressions;
+
+namespace ExpressionTreeToString.Tests {
+    partial class DynamicLinqTestObjects {
+        [TestObject("OperationGrouping")]
+        internal static readonly Expression HigherBinaryPrecedence = Expr("(@0 + @1) * @2", 1, 2, 3);
+
+        [TestObject("OperationGrouping")]
+        internal static readonly Expression BinaryPrecedence = Expr("(@0 + @1).ToString()", 1, 2);
+    }
+}
